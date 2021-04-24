702cb3302626db0a34e056993697398208d0a754
KqkaricK <andrew73-42@yandex.ru>
2021-04-24 22:21:20 +0700

    Добавьте файлы проекта.

---
+++ OOPLab6/InterfaceGeometry.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLab6
+{
+    public interface InterfaceGeometry
+    {
+        double PerimetrTraingle();
+        double AreaTraingle();
+        double PerimetrRectangle();
+        double AreaRectangle();
+    }
+    class MainGeometry : InterfaceGeometry
+    {
+        double My_x1, My_y1, My_x2, My_y2, My_x3, My_y3, My_x4, My_y4;
+        public double x1
+        {
+            set { My_x1 = value; }
+            get { return My_x1; }
+        }
+        public double y1
+        {
+            set { My_y1 = value; }
+            get { return My_y1; }
+        }
+        public double x2
+        {
+            set { My_x2 = value; }
+            get { return My_x2; }
+        }
+        public double y2
+        {
+            set { My_y2 = value; }
+            get { return My_y2; }
+        }
+        public double x3
+        {
+            set { My_x3 = value; }
+            get { return My_x3; }
+        }
+        public double y3
+        {
+            set { My_y3 = value; }
+            get { return My_y3; }
+        }
+        public double x4
+        {
+            set { My_x4 = value; }
+            get { return My_x4; }
+        }
+        public double y4
+        {
+            set { My_y4 = value; }
+            get { return My_y4; }
+        }
+        public MainGeometry() { }
+        public MainGeometry(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+        }
+        public MainGeometry(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+            this.x4 = x4;
+            this.y4 = y4;
+        }
+        public virtual double PerimetrTraingle()
+        {
+            double Dlina1 = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double Dlina2 = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double Dlina3 = Math.Sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+            return Dlina1 + Dlina2 + Dlina3;
+        }
+        public virtual double AreaTraingle()
+        {
+            return Math.Abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+        }
+        public virtual double PerimetrRectangle()
+        {
+            double Dlina1 = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double Dlina2 = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double Dlina3 = Math.Sqrt((x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4));
+            double Dlina4 = Math.Sqrt((x4 - x1) * (x4 - x1) + (y4 - y1) * (y4 - y1));
+            return Dlina1 + Dlina2 + Dlina3 + Dlina4;
+        }
+        public virtual double AreaRectangle()
+        {
+            double Dlina1 = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double Dlina2 = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+            double Dlina3 = Math.Sqrt((x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4));
+            double Dlina4 = Math.Sqrt((x4 - x1) * (x4 - x1) + (y4 - y1) * (y4 - y1));
+            double PoluPer = (Dlina1 + Dlina2 + Dlina3 + Dlina4) / 2;
+            return Math.Round(Math.Sqrt((PoluPer - Dlina1) * (PoluPer - Dlina2) * (PoluPer - Dlina3) * (PoluPer - Dlina4)));
+        }
+    }
+}
---
+++ OOPLab6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOPLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2, x3, y3, x4, y4;
+            Console.Write("X1 = "); //0
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y1 = ");//1
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X2 = ");//3
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y2 = ");//1
+            y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X3 = ");//3
+            x3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y3 = ");//-3
+            y3 = Convert.ToDouble(Console.ReadLine());
+            MainGeometry A = new MainGeometry(x1, y1, x2, y2, x3, y3);
+            Console.WriteLine(A.PerimetrTraingle());
+            Console.WriteLine(A.AreaTraingle());
+            Console.Write("X1 = ");//4
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y1 = ");//2
+            y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X2 = ");//8
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y2 = ");//4
+            y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X3 = ");//6
+            x3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y3 = ");//8
+            y3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X4 = ");//2
+            x4 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y4 = ");//6
+            y4 = Convert.ToDouble(Console.ReadLine());
+            A = new MainGeometry(x1, y1, x2, y2, x3, y3, x4, y4);
+            Console.WriteLine(A.PerimetrRectangle());
+            Console.WriteLine(A.AreaRectangle());
+            Console.ReadKey();
+        }
+    }
+}
